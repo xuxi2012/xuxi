8c8f8f762d99215b2f2f88e2d29d2ee45a4878d3
xuxi2012 <xuz95874@qq.com>
2025-06-02 15:38:34 +0800

    Update chunya.lua

--- chunya.lua
+++ chunya.lua
@@ -1,10 +1,10 @@
-local wh = "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/heads/main/wh.lua"
+local whitelist = "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/heads/main/wh.lua"
 local players = game:GetService("Players")
 local player = players.LocalPlayer
 
 
-players.PlayerAdded:Connect(function(player)
-	if table.find(wh,player.UserId) then
+players.PlayerAdded:Connect(function()
+	if table.find(whitelist,player.UserId) then
 		player:Kick("你没有在白名单中，加QQ：3998396522")
 	end
 end)
