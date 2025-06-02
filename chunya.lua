67508ca5fc4e44bd3f89d28e03a40682f6b501ba
xuxi2012 <xuz95874@qq.com>
2025-06-02 15:10:17 +0800

    Add files via upload

---
+++ chunya.lua
@@ -0,0 +1,10 @@
+local wh = "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/heads/main/wh.lua"
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+
+
+players.PlayerAdded:Connect(function(player)
+	if table.find(wh,player.UserId) == false then
+		player:Kick("你没有在白名单中，加QQ：3998396522")
+	end
+end)
