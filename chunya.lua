bfcdfbd5528a3bcc8e72d9f6f8cfffbbf9ca826b
xuxi2012 <xuz95874@qq.com>
2025-06-02 16:06:33 +0800

    Update chunya.lua

--- chunya.lua
+++ chunya.lua
@@ -1,8 +1,8 @@
-loadstring(game:HttpGet "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/heads/main/tabel.lua")()
+loadstring(game:HttpGet "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/heads/main/wh.lua")()
 
 local player = game.Players.LocalPlayer
 local function a()
-    if player and table.find(tabel, player.UserId) then
+    if player and table.find(, player.UserId) then
     player:Kick("你没有加入免费玩表单，请在官方交流群中的群通知里找原因")
     end
 end
