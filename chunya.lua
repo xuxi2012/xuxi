0d0dcf4c760bc609fe22cb107e0e8bed58046140
xuxi2012 <xuz95874@qq.com>
2025-06-02 16:07:14 +0800

    Update chunya.lua

--- chunya.lua
+++ chunya.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet "https://raw.githubusercontent.com/xuxi2012/xuxi/refs/he
 
 local player = game.Players.LocalPlayer
 local function a()
-    if player and table.find(, player.UserId) then
+    if player and table.find(whitelist, player.UserId) then
     player:Kick("你没有加入免费玩表单，请在官方交流群中的群通知里找原因")
     end
 end
